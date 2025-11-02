56d823326efa45c402536787e726e294567257a1
Kobus Botha <jjbotha@shoprite.co.za>
2025-11-02 15:21:27 +0200

    Added client conroller

--- CPR.API/Models/Result.cs
+++ CPR.API/Models/Result.cs
@@ -16,4 +16,19 @@
         public static Result<T> Ok(T value, string msg = "") => new(true, value, string.IsNullOrEmpty(msg) ? "Operation successful" : msg);
         public static Result<T> Fail(string error) => new(false, default, error);
     }
+
+    public class Result
+    {
+        public bool IsSuccess { get; }
+        public string Message { get; }
+
+        private Result(bool isSuccess, string message)
+        {
+            IsSuccess = isSuccess;
+            Message = message;
+        }
+
+        public static Result Ok(string msg = "") => new(true, string.IsNullOrEmpty(msg) ? "Operation successful" : msg);
+        public static Result Fail(string error) => new(false, error);
+    }
 }
